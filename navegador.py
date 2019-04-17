b12be6335f1e1a147b221b52c6f0c07ac10a61c5
Paulo Vilela <49640099+paulofv@users.noreply.github.com>
2019-04-17 13:16:36 -0300

    Add files via upload

---
+++ navegador.py
@@ -0,0 +1,84 @@
+﻿#coding: utf-8
+
+import sys
+import socket
+import os
+
+PORT = 80 #porta padrão caso usuário não especifique
+
+if len(sys.argv) == 1:
+    print "Nao foi informado a url no paramentro!\n"
+    sys.exit()
+    
+if len(sys.argv) == 3:
+	PORT = int(sys.argv[2])
+	
+link = sys.argv[1].replace("https://", "").replace("http://", "") #remover http:// do link
+
+HOST = link.split('/') #separar o host do resto da url
+HOST = HOST[0]
+
+socketnav = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socketnav.connect((HOST, PORT))
+
+print "Conectando a " + HOST + "\n\n"
+
+url = link.split('/')
+
+nomearq = ''
+
+if len(url) > 1:
+	nomearq = url[len(url)-1] #recebe o nome do arquivo caso seja especificado
+	
+del url[0]
+caminho = ''
+
+if type(url) == list:
+	for i in url:
+		caminho += '/' + i #monta o caminho para ser feita a requisicao
+else:
+	caminho = '/'
+	
+if len(nomearq) == 0:
+	nomearq = "index.html"
+
+requisicao = 'GET '+ caminho + ' HTTP/1.0\r\nHost: ' + HOST + '\r\n\r\n'
+socketnav.sendall(requisicao)
+
+pagina = ''
+
+while (True):
+	rcv = socketnav.recv(4096)
+	if not rcv:
+		break
+	pagina += rcv
+
+linhas = pagina.split('\n'); #separa o conteudo recebido em linhas
+
+i = 0
+while(True):  #separa o cabecalho da Cabeçalho HTTP do conteudo
+	if len(linhas[i]) == 1:
+		inicio = i + 1
+		break
+	i += 1
+
+dir = './' + HOST #pasta pra salvar o conteudo
+if not os.path.isdir(dir): #verifica se ja existe, se nao existe cria
+	os.makedirs(dir)
+
+arqsaida = dir + '/' + nomearq
+
+arquivo_saida = open(arqsaida,"w") #criando arquivo de saida
+	
+i = 0
+print "Cabeçalho HTTP"
+while(i < inicio):  #imprimindo o Cabeçalho HTTP na tela
+	print(linhas[i])
+	i += 1
+	
+while (inicio < len(linhas)):  #salvando o conteudo no arquivo
+	arquivo_saida.write(linhas[inicio])
+	arquivo_saida.write("\n")
+	inicio += 1
+
+socketnav.close
---
+++ servidor.py
@@ -0,0 +1,51 @@
+﻿#coding: utf-8
+
+import socket
+import thread
+import sys
+import os
+
+HOST = 'localhost' 		# endereco ip do servidor
+PORT = 6666 			# porta que o servidor esta
+
+if len(sys.argv) == 1:
+	print "Nenhuma pasta passada por parametro!\nEncerrando servidor...\n"
+
+if len(sys.argv) == 3:
+	PORT = int(sys.argv[2])
+
+pastaserv = sys.argv[1]
+
+def existe_arquivo(arquivo):
+	caminho = pastaserv + '/' + arquivo
+	return os.path.isfile(caminho)
+	
+def conectado(con, cliente):
+	print "Conectado ao cliente ", cliente
+	
+	mensagem = con.recv(1024)
+	arquivo = mensagem.split(' ')
+	arqsolicitado = arquivo[1]
+	print arqsolicitado
+	if existe_arquivo(arqsolicitado):
+		f = open(pastaserv + arqsolicitado)
+		cabecalho = 'HTTP/1.0 200 OK\r\nExpires: -1\r\n \n'
+		arq = f.read()
+		envia = cabecalho + arq
+		con.sendall(envia)
+		con.close()
+	else:
+		con.send("Erro 404 - Arquivo nao encontrado")
+		con.close()
+	print "Fechando conexao com o cliente ", cliente
+
+
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp.bind((HOST, PORT)) 	# associa o socket a uma porta e um IP
+tcp.listen(1)			# coloca o servidor escutando (modo passivo)
+print("Servidor iniciado no endereço: http://%s porta: %d" %(HOST, PORT))
+
+while True:
+	con, cliente = tcp.accept() # retorna um objeto socket novo e o endereço do cliente
+	thread.start_new_thread(conectado, tuple([con, cliente]))
+tcp.close()

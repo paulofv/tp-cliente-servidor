8b5d05ae888c983b5a39233a16fc93f3070e497f
Paulo Vilela <49640099+paulofv@users.noreply.github.com>
2019-05-24 15:48:33 -0300

    Add files via upload

--- servidor.py
+++ servidor.py
@@ -13,25 +13,27 @@ def conectado(con, cliente):
 	print "Conectado ao cliente ", cliente
 	
 	mensagem = con.recv(1024)
+	
 	arquivo = mensagem.split(' ')
+	
 	arqsolicitado = arquivo[1]
 	print arqsolicitado
 	if existe_arquivo(arqsolicitado):
 		f = open(pastaserv + arqsolicitado)
-		cabecalho = 'HTTP/1.0 200 OK\r\nExpires: -1\r\n \n'
+		cabecalho = 'HTTP/1.0 200 OK\r\nConnection: keep-alive\r\nExpires: -1\r\n\r\n'
 		arq = f.read()
 		envia = cabecalho + arq
 		con.sendall(envia)
 		con.close()
 	else:
-		cabecalho = 'HTTP/1.0 404 Not Found\r\nExpires: -1\r\n \n'
-		envia = cabecalho + 'Arquivo nao encontrado!'
+		cabecalho = 'HTTP/1.0 404 Not Found\r\nExpires: -1\r\n\n'
+		envia = '404 Not Found'
 		con.send(envia)
 		con.close()
 	print "Fechando conexao com o cliente ", cliente
 
 HOST = 'localhost' 		# Endereco do servidor
-PORT = 6666 			# Porta padrao caso nao seja especificada nos parametros
+PORT = 8800 			# Porta padrao caso nao seja especificada nos parametros
 
 if len(sys.argv) == 1:  # Verifica se o usuario passou pelo menos o parametro obrigatorio
 	print "Nenhuma pasta passada por parametro!\nEncerrando servidor...\n"
